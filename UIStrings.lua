54dea2726fa24268a9fa64496b0df866fa0d579f
Mike Babcock <michaelcbabcock@gmail.com>
2026-04-02 15:08:22 -0400

    Update UIStrings.lua

--- UIStrings.lua
+++ UIStrings.lua
@@ -295,6 +295,10 @@ PawnUIFrame_OptionsHeaderLabel_Text = L.OptionsWelcome
 -- Custom: Actual vs Optimized Gem Scoring
 PawnUIString_UseActualSocketedGems = "Use actual socketed gems"
 PawnUIString_UseActualSocketedGemsTooltip = "When enabled, Pawn scores items using the gems actually socketed in the item instead of optimized gem values."
+
+PawnUIFrame_UseActualSocketedGemsCheck_Text = PawnUIString_UseActualSocketedGems
+PawnUIFrame_UseActualSocketedGemsCheck_Tooltip = PawnUIString_UseActualSocketedGemsTooltip
+
 PawnUIString_GemValueActual = "Gem value (actual)"
 PawnUIString_GemValueOptimized = "Gem value (optimized)"
 
